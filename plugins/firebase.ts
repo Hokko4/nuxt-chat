e0e2610e5db8a2bc593abbafc050792213109160
Hokko4 <wondermelon0715@gmail.com>
2020-01-05 21:37:03 +0900

    firebase config

---
+++ plugins/firebase.ts
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyBGMDTh6BW1ZknjeNcMADzPW-QWCJfH67k',
+    authDomain: 'my-nuxt-chat-4e318.firebaseapp.com',
+    databaseURL: 'https://my-nuxt-chat-4e318.firebaseio.com',
+    projectId: 'my-nuxt-chat-4e318',
+    storageBucket: 'my-nuxt-chat-4e318.appspot.com',
+    messagingSenderId: '133488783417',
+    appId: '1:133488783417:web:f42728168d738a86f89de8',
+    measurementId: 'G-TM664EX543'
+  })
+}
+
+export default firebase
